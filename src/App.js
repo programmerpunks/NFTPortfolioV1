b18f13f6771791e81fb816e706da411c8dea1bb6
GMw3 <w3ghulam@gmail.com>
2022-12-29 18:54:59 +0500

    Added latest git tokken for setting url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">NFT Portfolio Version 1</h1>
+      <h1 className="text-3xl font-bold underline">NFT Portfolio Version 1 Mine</h1>
     </>
   );
 }
