bf8556f8592b6be42bf0617c563e30c616da610d
GMw3 <w3ghulam@gmail.com>
2023-04-10 12:53:57 +0500

    Added Minting Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import About from "./components/about";
+import { ContractABI } from "./components/mint/Contract";
 import Footer from "./components/footer/Footer";
 import Hero from "./components/hero";
+import Mint from "./components/mint";
 import Navbar from "./components/navbar";
 import NavBarDetail from "./components/navbarDetail";
 import RoadMap from "./components/roadmap/RoadMap";
@@ -11,18 +17,143 @@ import Team from "./components/team/Team";
 function App() {
   const [nav, setNav] = useState(false);
 
+  const [images, setImages] = useState([]);
+  const [logout, setLogout] = useState(false);
+  const [maxMintAmount, setMaxMintAmount] = useState();
+  const [price, setPrice] = useState(0);
+  const [userMintedAmount, setUserMintedAmount] = useState(0);
+  const [wallet, setWallet] = useState("Connect a Wallet");
+
+  const { REACT_APP_CONTRACT_ADDRESS } = process.env;
+  const { REACT_APP_NETWORK } = process.env;
+  const { REACT_APP_NETWORK_CHAIN_ID } = process.env;
+
+  const connection = async () => {
+    const res = await setupConnections();
+    if (res === false) {
+      setWallet("Connnect a Wallet");
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: REACT_APP_NETWORK_CHAIN_ID }],
+      });
+    } else {
+      setLogout(true);
+      setWallet(res.slice(0, 6) + "..." + res.slice(36, 42));
+    }
+  };
+
+  const disconnect = async () => {
+    setWallet("Connect a Wallet");
+    setLogout(false);
+    setUserMintedAmount(0);
+    setMaxMintAmount("-");
+    setPrice("-");
+    setImages([]);
+  };
+
+  const getTokens = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      REACT_APP_CONTRACT_ADDRESS,
+      ContractABI,
+      provider
+    );
+    let accounts = await provider.send("eth_requestAccounts", []);
+    let address = accounts[0];
+    const imagesTockens = await contract.nftsOnwedByWallet(address);
+    let imagesLocal = [];
+    await imagesTockens.map(async (image) => {
+      const url = await contract.tokenURI(parseInt(image, 10));
+      let result = await url.replace("ipfs://", "https://ipfs.io/ipfs/");
+      const jsonBody = await (await fetch(result)).json();
+      imagesLocal.push(
+        await jsonBody.image.replace("ipfs://", "https://ipfs.io/ipfs/")
+      );
+    });
+    setTimeout(() => {
+      setImages(imagesLocal);
+    }, [3000]);
+  };
+  const notify = (message) => {
+    toast.error(message, {
+      toastId: "custom-id-yes",
+    });
+  };
+
+  const readContract = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      REACT_APP_CONTRACT_ADDRESS,
+      ContractABI,
+      provider
+    );
+    const maxMintAmount = await contract.maxMintAmount();
+    let accounts = await provider.send("eth_requestAccounts", []);
+    let address = accounts[0];
+    const userMintedAmount = await contract.balanceOf(address);
+    const price = await contract.cost();
+    setMaxMintAmount(parseInt(maxMintAmount, 10));
+    setUserMintedAmount(parseInt(userMintedAmount, 10));
+    setPrice(Number(ethers.utils.formatEther(price)));
+  };
+
+  const setupConnections = async () => {
+    if (window.ethereum != null) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const network = await provider.getNetwork();
+      if (REACT_APP_NETWORK !== network.name) {
+        notify(
+          `Not on a correct network. Change your network to "${REACT_APP_NETWORK}"`
+        );
+        return false;
+      } else {
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        return address;
+      }
+    } else {
+      notify("No Ether wallet available");
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    connection();
+  }, []);
+
   return (
     <>
       {nav ? (
         <NavBarDetail setValue={setNav} />
       ) : (
         <div className="bg-gradient-to-r from-[#75013f] to-black">
-          <Navbar setValue={setNav} />
+          <Navbar
+            setValue={setNav}
+            connection={connection}
+            disconnect={disconnect}
+            getTokens={getTokens}
+            logout={logout}
+            readContract={readContract}
+            wallet={wallet}
+          />
           <Hero />
           <About />
           <RoadMap />
           <Team />
+          <Mint
+            connection={connection}
+            disconnect={disconnect}
+            getTokens={getTokens}
+            images={images}
+            maxMintAmount={maxMintAmount}
+            price={price}
+            readContract={readContract}
+            userMintedAmount={userMintedAmount}
+            wallet={wallet}
+          />
           <Footer />
+          <ToastContainer position="top-center" autoClose={2000} />
         </div>
       )}
     </>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,18 +3,53 @@ import { CgMenuLeft } from "react-icons/cg";
 
 import { navbarData } from "../../content/Navbar";
 
-const Navbar = ({ setValue }) => {
+const Navbar = ({
+  setValue,
+  connection,
+  disconnect,
+  getTokens,
+  logout,
+  readContract,
+  wallet,
+}) => {
   return (
     <>
-      <div className="flex  bg-transparent justify-between p-2 px-10 fixed z-30 top-0 w-full">
-        <div className="w-[10%] items-center flex">
+      <div className="flex  bg-transparent  backdrop-blur-[80px] md:backdrop-blur-0  justify-between p-2 md:px-10 fixed z-30 top-0 w-full">
+        <div className="w-[30%] md:w-[10%] items-center flex">
           <img src={navbarData.logo} alt="/" />
         </div>
-        <CgMenuLeft
-          size={35}
-          className="text-white m-2 cursor-pointer"
-          onClick={() => setValue(true)}
-        />
+        <div className="flex text-white">
+          <div className="py-2 ml-3 flex items-center justify-center ">
+            <button
+              onClick={async () => {
+                logout
+                  ? (async function () {
+                      await disconnect();
+                    })()
+                  : (async function () {
+                      await connection();
+                      await readContract();
+                      await getTokens();
+                    })();
+              }}
+              className={`flex border-[1px] border-white p-1 hover:scale-125 duration-700 rounded-lg px-2 justify-center  text-white bg-transparent cursor-pointer ${
+                logout ? "hover:before:content-['Disconnect:']" : ""
+              }`}
+            >
+              <p
+                className={`h-[12px] w-[12px] border ${
+                  logout ? "bg-green-600" : "bg-red-600"
+                }  rounded-full flex items-center`}
+              ></p>
+              {wallet}
+            </button>
+          </div>
+          <CgMenuLeft
+            size={35}
+            className="text-white m-2 cursor-pointer"
+            onClick={() => setValue(true)}
+          />
+        </div>
       </div>
     </>
   );
